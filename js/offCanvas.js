904eef0e9d8a72db4636c262ea948e722ede056b
Inbar Tamir <inbartamir92@gmail.com>
2020-10-16 00:35:45 +0300

    Contact Form

--- js/offCanvas.js
+++ js/offCanvas.js
@@ -2,3 +2,17 @@ function openCanvas(){
     document.querySelector('.offcanvas-btn').classList.toggle('offcanvas-btn-open');
     document.querySelector('.offcanvas-aside').classList.toggle('offcanvas-aside-open');    
 }
+
+function sendMessage(ev) {
+    ev.preventDefault();
+    
+    var subject = document.querySelector('.offcanvas-aside input[name="subject"]').value;
+    var message = document.querySelector('.message').value;
+    var email = 'inbartamir92@gmail.com';
+
+    $('#contact form').trigger('reset');
+    openCanvas();
+    message = message.replace(/\n/g, '%0D%0A');
+    window.open(`https://mail.google.com/mail/u/0/?view=cm&fs=1&to=${email}&su=${subject}&body=${message}`);
+
+  }
